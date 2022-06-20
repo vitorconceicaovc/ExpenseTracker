3da29fb46c7fbde83735992674c42e2919da2896
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-06-20 14:11:01 +0100

    github

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,7 @@ const Header = () => {
     <C.Container>
         <C.Header>
         <C.Title>Expense Tracker</C.Title>
+        <C.Github><a href='https://github.com/vitorconceicaovc/ExpenseTracker' target="_blank" style={{textDecoration: 'none', color: 'lightgray'}} >Github</a></C.Github>
         </C.Header>
     </C.Container>
   )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,12 +4,36 @@ export const Container = styled.div`
     height: 150px;
     text-align: center;
     background: teal;
+    
 `;
 
-export const Header = styled.h1``;
+export const Header = styled.h1`
+`;
 
 
 export const Title = styled.div`
     padding-top: 20px;
     color: #fff;
+`;
+
+export const Github = styled.a`
+    text-decoration: none;  
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    border: 2px solid lightgray;
+    border-radius: 4px;
+    padding: 2px 10px;
+    font-size: 20px;
+    transition: all .4s ease;
+
+    :hover {
+        
+        border: 2px solid transparent;
+    }
+
+    
+
+
+
 `;
